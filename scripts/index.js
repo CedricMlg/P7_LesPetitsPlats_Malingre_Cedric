33bc4995d6fadd0eafd2ef45559c1a51cddc93e0
CedricMlg <malingre.cedric@gmail.com>
2022-09-06 15:50:01 +0200

    Add openable tag selector

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,26 @@
 import { recipes } from "../data/recipes.js";
 import { FactoryRecipeCard } from "./factories/recipeCard.js";
+import { FactoryTagSelector } from "./factories/tagSelector.js";
 
-window.addEventListener("load", new FactoryRecipeCard(recipes));
+const tagSelector = document.querySelectorAll(".header__tag-selector");
+
+tagSelector.forEach((element) => {
+  element.addEventListener("click", () => {
+    const selectorResearch = element.querySelector(".header__tag-researcher");
+    const selectorDisplay = element.querySelector(
+      ".header__tag-choice--display"
+    );
+
+    selectorResearch.querySelector("p").classList.add("active");
+    selectorResearch.querySelector("input").classList.add("active");
+    selectorResearch.querySelector("svg").classList.add("active");
+    selectorDisplay.classList.add("active");
+  });
+});
+
+window.addEventListener("load", () => refreshTagAndRecipe(recipes));
+
+function refreshTagAndRecipe(recipeData) {
+  new FactoryRecipeCard(recipeData);
+  new FactoryTagSelector(recipeData);
+}
