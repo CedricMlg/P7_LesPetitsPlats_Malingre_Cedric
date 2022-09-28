2556942998723297327b9a208502f2fca9debf4f
CedricMlg <malingre.cedric@gmail.com>
2022-09-28 15:15:18 +0200

    Fix research issues with accent and capital letter

--- scripts/components/ResearchBar.js
+++ scripts/components/ResearchBar.js
@@ -15,19 +15,34 @@ class ResearchBar {
 
     let result = [];
     result = researchArray.filter(
-      (item) => item.description.indexOf(`${this.formatedInput}`) !== -1
+      (item) =>
+        item.description
+          .toLowerCase()
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .indexOf(`${this.formatedInput}`) !== -1
     );
 
     if (result.length === 0) {
       for (const recipe of researchArray) {
         result = recipe.ingredients.filter(
-          (item) => item.ingredient.indexOf(`${this.formatedInput}`) !== -1
+          (item) =>
+            item.ingredient
+              .toLowerCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .indexOf(`${this.formatedInput}`) !== -1
         );
       }
 
       if (result.length === 0) {
         result = researchArray.filter(
-          (item) => item.name.indexOf(`${this.formatedInput}`) !== -1
+          (item) =>
+            item.name
+              .toLowerCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .indexOf(`${this.formatedInput}`) !== -1
         );
       }
     }
